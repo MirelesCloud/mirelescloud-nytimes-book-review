c36696dabf23950c6628b778ba4ac879433b7ac3
Alex Mireles <anchises1968@gmail.com>
2020-02-01 12:58:55 -0800

    Updates

---
+++ precache-manifest.893d463f0ebbfa2a09e895cf764b2f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b271a92f2309a3a8c5a8f5985a30688",
+    "url": "mirelescloud.github.io/mirelescloud-nytimes-book-review/index.html"
+  },
+  {
+    "revision": "76556d955c388eb229ed",
+    "url": "mirelescloud.github.io/mirelescloud-nytimes-book-review/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c440c74058946710209",
+    "url": "mirelescloud.github.io/mirelescloud-nytimes-book-review/static/js/2.39701a38.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "mirelescloud.github.io/mirelescloud-nytimes-book-review/static/js/2.39701a38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76556d955c388eb229ed",
+    "url": "mirelescloud.github.io/mirelescloud-nytimes-book-review/static/js/main.ff101ab5.chunk.js"
+  },
+  {
+    "revision": "7ce9405b4a777a00ab1b",
+    "url": "mirelescloud.github.io/mirelescloud-nytimes-book-review/static/js/runtime-main.754c3a41.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94a273658bfc18150642b6bb32147da1.js"
+  "mirelescloud.github.io/mirelescloud-nytimes-book-review/precache-manifest.893d463f0ebbfa2a09e895cf764b2f40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("mirelescloud.github.io/mirelescloud-nytimes-book-review/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
